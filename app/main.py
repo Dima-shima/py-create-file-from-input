7b895368efdcf3553f1c99a1c3a715862da0ecd0
Dima-shima <shifrin1970@gmail.com>
2026-05-15 10:39:59 +0300

    implemented create-file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def main():
-    # write your code here
-    pass
+    file_name = input("Enter file name: ")
+    strings = []
+    string = ""
+    while not string == "stop" or string == "STOP":
+        strings.append(string)
+        string = input("Enter new string: ")
+        if not string == "stop" or string == "":
+            strings += "\n"
+    strings = strings[2:]
+    print(strings)
+    result = "".join(strings)
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(result)
 
 
 if __name__ == "__main__":
